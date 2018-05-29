7f34c41ffe8d14ba8cfaffc4ad65ef75d543ceb7
Mikko Luhtasaari <mikko.luhtasaari@cybercom.com>
2018-05-29 14:56:03 +0300

    Add start of UI

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,14 +1,40 @@
 import React, {Component} from "react"
-import {View, Text, StyleSheet} from "react-native"
+import {View, Text, StyleSheet, TextInput} from "react-native"
+import {black, blue, white} from "../utils/colors";
 
 class NewDeck extends Component {
+    state = {
+        inputText: "Insert deck title here!"
+    };
     render() {
         return (
-            <View>
-                <Text>New Deck</Text>
+            <View style={styles.container}>
+                <Text style={styles.title}>Title here!</Text>
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(text) => this.setState({text})}
+                    value={this.state.text}
+                    editable={true}
+                    autoCorrect={false}
+                    clearTextOnFocus={true}
+                />
+                {/*TODO Add submit button and handle the action.*/}
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: white
+    },
+    title: {
+        textAlign: "center",
+        fontSize: 30,
+        color: black
+    }
+});
 
 export default NewDeck
