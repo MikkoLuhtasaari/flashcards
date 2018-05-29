3d969a61aa4be89db49a34b93f11cec1138bcf63
Mikko Luhtasaari <mikko.luhtasaari@cybercom.com>
2018-05-29 10:32:54 +0300

    Add basic navigation

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Constants} from "expo"
+import {TabNavigator, createStackNavigator, createMaterialTopTabNavigator} from "react-navigation"
+import {StyleSheet, Text, View, StatusBar, Platform} from 'react-native';
+import {purple, white} from "./utils/colors"
+import DeckList from "./components/DeckList"
+import NewDeck from "./components/NewDeck"
+import {MaterialCommunityIcons, FontAwesome, Ionicons} from "@expo/vector-icons"
 
-export default class App extends React.Component {
-  render() {
+function UdaciStatusBar({backgroundColor, ...props}) {
+
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+        <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+            <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const Tabs = createMaterialTopTabNavigator({
+        DeckList: {
+            screen: DeckList,
+            navigationOptions: {
+                tabBarLabel: "Deck List",
+                tabBarIcon: ({tintColor}) => <MaterialCommunityIcons name="cards-outline" size={30} color={tintColor}/>
+            }
+        },
+        NewDeck: {
+            screen: NewDeck,
+            navigationOptions: {
+                tabBarLabel: "New Deck",
+                tabBarIcon: ({tintColor}) => <FontAwesome name="plus-square" size={30} color={tintColor}/>
+            }
+        }
+    },
+    {
+        navigationOptions: {
+            header: null
+        },
+        tabBarOptions: {
+            activeTintColor: Platform.OS === "ios" ? purple : white,
+            style: {
+                height: 56,
+                backgroundColor: Platform.OS === "ios" ? white : purple,
+                shadowRadius: 6,
+                shadowOpacity: 1,
+                shadowColor: "rgba(0,0,0,0.24)",
+                shadowOffset: {
+                    width: 0,
+                    height: 3
+                }
+            }
+        }
+    });
+
+const MainNavigator = createStackNavigator({
+    Home: {
+        screen: Tabs
+    }
 });
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
+                <MainNavigator/>
+            </View>
+        );
+    }
+}
