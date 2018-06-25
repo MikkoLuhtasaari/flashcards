412d20f0896d3200dd87c2b4ce8e470f99849f34
Mikko Luhtasaari <mikko.luhtasaari@cybercom.com>
2018-06-25 12:58:30 +0300

    Add sample texts and checkbox

--- components/AddCardToDeck.js
+++ components/AddCardToDeck.js
@@ -3,41 +3,40 @@ import {View, Text, StyleSheet, TextInput, TouchableOpacity} from "react-native"
 import {black, blue, white} from "../utils/colors";
 import {addCardToDeck} from "../utils/api";
 import {StackActions, NavigationActions} from "react-navigation"
+import {CheckBox} from "react-native-elements"
 
 class AddCardToDeck extends Component {
     state = {
         question: "Insert question",
         answer1: "Insert first answer",
-        answer2: "Insert second answer"
+        answer2: "Insert second answer",
+        checked: true
     };
 
     toHome = () => {
         this.props.navigation.dispatch(StackActions.reset({
             index: 0,
             actions: [
-                NavigationActions.navigate({ routeName: 'Home'})
+                NavigationActions.navigate({routeName: 'Home'})
             ]
         }))
     };
 
     submit = () => {
-        const {question, answer1, answer2} = this.state;
+        const {question, answer1, answer2, checked} = this.state;
         const deckName = this.props.navigation.state.params.deck;
         const formattedQuestion = {
-            "question": {
-                "title": question,
-                "answers": {
-                    "answer1": answer1,
-                    "answer2": answer2
-                }
-            }
+            "question": question,
+            "answer1": answer1,
+            "answer2": answer2,
+            "firstAnswerCorrect": checked
         };
         this.setState(() => ({
             question: "",
             answer1: "",
             answer2: ""
         }));
-        console.log("Deckname " + deckName + " and Formatted question " + formattedQuestion.toString());
+        console.log("Deckname " + deckName + " and Formatted question " + Object.values(formattedQuestion));
         this.toHome();
         // addCardToDeck().then((returnValue) => {
         //     this.toHome()
@@ -52,6 +51,7 @@ class AddCardToDeck extends Component {
                 <TextInput
                     style={{height: 40, borderColor: 'gray', borderWidth: 1}}
                     onChangeText={(question) => this.setState({question})}
+                    value={this.state.question}
                     editable={true}
                     autoCorrect={false}
                     clearTextOnFocus={true}
@@ -59,6 +59,7 @@ class AddCardToDeck extends Component {
                 <TextInput
                     style={{height: 40, borderColor: 'gray', borderWidth: 1}}
                     onChangeText={(answer1) => this.setState({answer1})}
+                    value={this.state.answer1}
                     editable={true}
                     autoCorrect={false}
                     clearTextOnFocus={true}
@@ -66,9 +67,17 @@ class AddCardToDeck extends Component {
                 <TextInput
                     style={{height: 40, borderColor: 'gray', borderWidth: 1}}
                     onChangeText={(answer2) => this.setState({answer2})}
+                    value={this.state.answer2}
                     editable={true}
                     autoCorrect={false}
                     clearTextOnFocus={true}
+                />
+                <CheckBox
+                    title='Is first answer correct'
+                    checkedIcon='dot-circle-o'
+                    uncheckedIcon='circle-o'
+                    checked={this.state.checked}
+                    onPress={() => this.setState({checked: !this.state.checked})}
                 />
                 <TouchableOpacity onPress={this.submit} style={{backgroundColor: black}}>
                     <Text style={{color: blue}}>Submit</Text>
